cb2a33f48a4c1e6e24d9d30e4d8019959f93c199
VadimShepelin <kocichca@bk.ru>
2024-10-15 14:01:23 +0300

    Fixed issues when working with Instant in the CreateSubscriptionMapperTest test class

--- src/test/java/com/dmdev/mapper/CreateSubscriptionMapperTest.java
+++ src/test/java/com/dmdev/mapper/CreateSubscriptionMapperTest.java
@@ -4,6 +4,7 @@ import com.dmdev.dto.CreateSubscriptionDto;
 import com.dmdev.entity.Provider;
 import com.dmdev.entity.Status;
 import com.dmdev.entity.Subscription;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
@@ -14,31 +15,33 @@ class CreateSubscriptionMapperTest {
 
     private final CreateSubscriptionMapper createSubscriptionMapper = CreateSubscriptionMapper.getInstance();
 
-    @Test
+    @RepeatedTest(5)
     void map() {
-        CreateSubscriptionDto dto = getCreateSubscriptionDto();
-        Subscription subscription = getSubscription();
+        Instant now = Instant.now();
+        CreateSubscriptionDto dto = getCreateSubscriptionDto(now);
+        Subscription subscription = getSubscription(now);
 
         Subscription actualResult = createSubscriptionMapper.map(dto);
 
         assertEquals(subscription, actualResult);
+        assertEquals(subscription.getExpirationDate().getNano(), actualResult.getExpirationDate().getNano());
     }
 
-    private Subscription getSubscription() {
+    private Subscription getSubscription(Instant now) {
         return Subscription.builder()
                 .userId(1)
                 .name("Ivan")
-                .expirationDate(Instant.ofEpochSecond(1000000,11))
+                .expirationDate(now.plusSeconds(10000))
                 .provider(Provider.APPLE)
                 .status(Status.ACTIVE)
                 .build();
     }
 
-    private CreateSubscriptionDto getCreateSubscriptionDto() {
+    private CreateSubscriptionDto getCreateSubscriptionDto(Instant now) {
         return CreateSubscriptionDto.builder()
                 .userId(1)
                 .name("Ivan")
-                .expirationDate(Instant.ofEpochSecond(1000000,11))
+                .expirationDate(now.plusSeconds(10000))
                 .provider("apple")
                 .build();
     }
